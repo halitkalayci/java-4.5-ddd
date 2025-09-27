c0422a366b363570ec050461e93734f899194c37
halitklyc <halitenes.kalayci@gmail.com>
2025-09-27 15:54:08 +0300

    Product ctor best practices

--- src/main/java/com/turkcell/ecommerce_cqrs/domain/Product.java
+++ src/main/java/com/turkcell/ecommerce_cqrs/domain/Product.java
@@ -21,17 +21,20 @@ public class Product
     // Bu nesne yalnızca benim vereceğim fabrika metotları ile oluşturulsun.
     private Product(ProductId id, String name, String description, Money money, Integer stock) {
         this.id = id;
-        setName(name);
-        setDescription(description);
-        setMoney(money);
-        setStock(stock);
+        this.name = name;
+        this.description = description;
+        this.money = money;
+        this.stock = stock;
     }
-
     public static Product create(String name, String description, Money money, Integer stock)
     {
+        validateName(name);
+        validateDescription(description);
+        Objects.requireNonNull(money, "Money cannot be null");
+        validateStock(stock);
+
         return new Product(ProductId.generate(), name, description, money, stock);
     }
-
     public void setName(String name)
     {
         validateName(name);
@@ -52,24 +55,43 @@ public class Product
         validateStock(stock);
         this.stock = stock;
     }
-
-    private void validateStock(Integer stock)
+    private static void validateStock(Integer stock)
     {
         if (stock == null || stock <= 0)
             throw new IllegalArgumentException("Stock cannot be null or negative value");
     }
-    private void validateDescription(String description)
+    private static void validateDescription(String description)
     {
         if(description == null || description.length() < 3)
             throw new IllegalArgumentException("Description must be at least 3 characters");
         if(description.length() > 255)
             throw new IllegalArgumentException("Description length must be less than 255 characters");
     }
-    private void validateName(String name)
+    private static void validateName(String name)
     {
         if(name == null || name.isEmpty())
             throw new IllegalArgumentException("Name cannot be null or empty");
         if(name.length() < 3)
             throw new IllegalArgumentException("Name must be at least 3 characters");
+    }
+
+    public ProductId id() {
+        return id;
+    }
+
+    public String name() {
+        return name;
+    }
+
+    public String description() {
+        return description;
+    }
+
+    public Money money() {
+        return money;
+    }
+
+    public Integer stock() {
+        return stock;
     }
 }
