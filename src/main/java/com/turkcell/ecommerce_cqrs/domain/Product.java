1d9936ad60d606ce5d33f1e20b96fc6664afaf3e
halitklyc <halitenes.kalayci@gmail.com>
2025-09-27 15:47:04 +0300

    change Product and ProductId

--- src/main/java/com/turkcell/ecommerce_cqrs/domain/Product.java
+++ src/main/java/com/turkcell/ecommerce_cqrs/domain/Product.java
@@ -1,6 +1,7 @@
 package com.turkcell.ecommerce_cqrs.domain;
 
 import java.math.BigDecimal;
+import java.util.List;
 import java.util.Objects;
 import java.util.UUID;
 
@@ -10,15 +11,59 @@ import java.util.UUID;
 // Aggregate
 public class Product
 {
-    private UUID id; // Bu özel bi yapı mı? Yoksa sadece değer mi?
+    private final ProductId id; // Bu özel bi yapı mı? Yoksa sadece değer mi?
     private String name;
     private String description;
     private Money money;
+    private Integer stock; // Integer -> Nesne, int (primitive type)
+
+    // Kontrollü Yaratım -> Ctor'ı private yapıyoruz
+    // Bu nesne yalnızca benim vereceğim fabrika metotları ile oluşturulsun.
+    private Product(ProductId id, String name, String description, Money money, Integer stock) {
+        this.id = id;
+        setName(name);
+        setDescription(description);
+        setMoney(money);
+        setStock(stock);
+    }
+
+    public static Product create(String name, String description, Money money, Integer stock)
+    {
+        return new Product(ProductId.generate(), name, description, money, stock);
+    }
 
     public void setName(String name)
     {
         validateName(name);
         this.name = name;
+    }
+    public void setMoney(Money money)
+    {
+        Objects.requireNonNull(money, "Money cannot be null");
+        this.money = money;
+    }
+    public void setDescription(String description)
+    {
+        validateDescription(description);
+        this.description = description;
+    }
+    public void setStock(Integer stock)
+    {
+        validateStock(stock);
+        this.stock = stock;
+    }
+
+    private void validateStock(Integer stock)
+    {
+        if (stock == null || stock <= 0)
+            throw new IllegalArgumentException("Stock cannot be null or negative value");
+    }
+    private void validateDescription(String description)
+    {
+        if(description == null || description.length() < 3)
+            throw new IllegalArgumentException("Description must be at least 3 characters");
+        if(description.length() > 255)
+            throw new IllegalArgumentException("Description length must be less than 255 characters");
     }
     private void validateName(String name)
     {
@@ -26,25 +71,5 @@ public class Product
             throw new IllegalArgumentException("Name cannot be null or empty");
         if(name.length() < 3)
             throw new IllegalArgumentException("Name must be at least 3 characters");
-    }
-
-    public void setMoney(Money money)
-    {
-        Objects.requireNonNull(money, "Money cannot be null");
-        this.money = money;
-    }
-
-    public void setDescription(String description)
-    {
-        validateDescription(description);
-        this.description = description;
-    }
-
-    private void validateDescription(String description)
-    {
-        if(description == null || description.length() < 3)
-            throw new IllegalArgumentException("Description must be at least 3 characters");
-        if(description.length() > 255)
-            throw new IllegalArgumentException("Description length must be less than 255 characters");
     }
 }
--- src/main/java/com/turkcell/ecommerce_cqrs/domain/ProductId.java
+++ src/main/java/com/turkcell/ecommerce_cqrs/domain/ProductId.java
@@ -1,4 +1,22 @@
 package com.turkcell.ecommerce_cqrs.domain;
 
-public class ProductId {
+import java.io.Serializable;
+import java.util.Objects;
+import java.util.UUID;
+
+// Bir yazılım nesnesi, "ağ üzerinde", "cache" içerisinde "dosya" sisteminde yazılabilir-okunabilir olmalıdır.
+public record ProductId(UUID value) implements Serializable {
+    public ProductId {
+        Objects.requireNonNull(value, "Value for ProductId cannot be null");
+    }
+
+    // Fabrika metodu
+    // Nesnenin kendi içinde kendi oluşturulma mekanizmasını yazması.
+    public static ProductId generate() {
+        return new ProductId(UUID.randomUUID());
+    }
 }
+
+// Serialize-Deserialize -> nesne => byte[] , byte[] -> nesne
+// abc.save -> Kod nesnelerinin serialize edilmiş hali
+// abc.save -> ProductId(); Deserialize
